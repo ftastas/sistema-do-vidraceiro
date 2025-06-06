d5f60aab64564dfd4d780ed7077243ca89bdf536
ftastas <natan-sn@hotmail.com>
2025-06-06 10:49:07 -0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,28 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import path from 'path'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src'),
-    },
-  },
-  server: {
-    host: true,
-    port: 5173,
-  },
+  base: '/',
   build: {
     outDir: 'dist',
+    assetsDir: 'assets',
     sourcemap: false,
-    minify: 'terser',
-    terserOptions: {
-      compress: {
-        drop_console: true,
-      },
-    },
-  },
+    minify: 'terser'
+  }
 })
-
